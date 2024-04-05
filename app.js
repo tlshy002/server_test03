c5d24f2bba6ba3314ea1adef05e201d7d9058c4d
HY <tlshy002@gmail.com>
2024-04-05 16:09:12 +0900

    Part1백업_ver01

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
 // 서버의 개념? 은 간단함 => 요청하는걸 갖다주는 프로그램
-
-
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
 
 // 몽고DB연결 세팅
 const { MongoClient } = require('mongodb')
 let db
-const url = 'mongodb+srv://user:hi30081227@cluster0.lyh4mwk.mongodb.net/?retryWrites=true&w=majority'
+const url = 'mongodb+srv://admin:qwer1234@cluster0.lyh4mwk.mongodb.net/?retryWrites=true&w=majority'
 new MongoClient(url).connect().then((client)=>{ // 몽고DB에 접속해줌
   console.log('DB연결성공')
-  db = client.db('forum') // 연결시킬 'DB이름'에 연결
-//   app.listen(4000, () => {
-//     console.log("http://localhost:4000에서 서버 실행중");
-//   })
+  db = client.db('forum') // 'DB이름'에 데이터베이스 연결
+    app.listen(4000, function() {
+        console.log('Server is running http://localhost:4000');
+    });
 }).catch((err)=>{
   console.log(err)
 })
 
+app.get('/list', async (req, res) => {
+    let result = await db.collection('post').find().toArray()
+    console.log(result[0].title)
+    // res.send(result[0].title);
+
+    res.render('list.ejs', { 글목록 : result });
+} )
+
+app.get('/time', (req, res) => {
+    let serverTime = new Date();
+    // console.log(serverTime)
+    res.render('time.ejs', { data : serverTime })
+})
+
+app.get('/time2', (요청, 응답) => {
+    응답.render('time.ejs', { data : new Date() })
+  })
+
+
+
+
 
 app.get('/pet', function(req, res) {
-    db.collection('post').insertOne( {title: 'a'})
+    db.collection('post').insertOne( {title: 'b'})
     res.send('환영합니다 펫용품 쇼핑 페이지입니다.');
 
 })
@@ -44,11 +64,3 @@ app.get('/about', function(req, res) {
 
 
 
-// (서버띄울 포트번호, 띄운후 실행할 코드)
-// 8080으로 들어왔을때 서버 열림
-// port? : 컴퓨터에는 외부와 네트워크 통신할 수 있는 통로가 있음
-app.listen(4000, function() {
-    // 8080port로 웹서버 열고 잘열리면 아래 코드 실행
-    console.log('Server is running http://localhost:4000');
-});
-// ex) /pet/home으로 get요청하면 펫상품 보여줌
